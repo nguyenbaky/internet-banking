2d49f47ed66caf069ba89deb6687a788c4b92068
nguyenbaky <43399274+nguyenbaky@users.noreply.github.com>
2020-06-30 19:42:13 +0700

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const httpSttCode = require('http-status-codes')
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 app.use(morgan('dev'))
 app.use(express.json())
